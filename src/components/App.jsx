309712644641ef3f7ac37aead4294b141e38838a
Devin Sweeting <devinsweeting@gmail.com>
2019-02-15 09:00:17 -0800

    attempt react particles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import { createGlobalStyle } from "styled-components";
 import Body from './Body';
+import Particles from 'react-particles-js';
+import styled from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -10,14 +12,25 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.section `
+  max-width: 100%;
+  height: 50em;
+  border: 1px solid black;
+`;
 
-function App(){
-  return (
-    <div>
-      <p>App Component</p>
-      <Body/>
-    </div>
-  );
+
+
+class App extends React.Component{
+  
+
+    render(){
+        return (
+          <Wrapper>
+            <Particles width="100em" height="100em"/>
+          </Wrapper>
+        );
+    };
+
 }
 
 export default App;
