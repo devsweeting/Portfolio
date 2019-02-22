51a03a9b97678277792bbec24b2d5d9ae8dde7f0
Devin Sweeting <devinsweeting@gmail.com>
2019-02-22 10:09:11 -0800

    style Arrow Down and center

--- src/components/Home/ParticlesView.jsx
+++ src/components/Home/ParticlesView.jsx
@@ -9,27 +9,27 @@ left: 0;
 width: 110%;
 height: 100%;
 
-background: linear-gradient(270deg, #040d24, #093e54, #065958, #04393d, #03362d);
+background: linear-gradient(304deg, #4662b8, #587be8, #52a3cf, #5fb8b2, #3ba16e);
 background-size: 1000% 1000%;
 
--webkit-animation: AnimationName 30s ease infinite;
--moz-animation: AnimationName 30s ease infinite;
-animation: AnimationName 30s ease infinite;
+-webkit-animation: AnimationName 32s ease infinite;
+-moz-animation: AnimationName 32s ease infinite;
+animation: AnimationName 32s ease infinite;
 
 @-webkit-keyframes AnimationName {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+    0%{background-position:0% 14%}
+    50%{background-position:100% 87%}
+    100%{background-position:0% 14%}
 }
 @-moz-keyframes AnimationName {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+    0%{background-position:0% 14%}
+    50%{background-position:100% 87%}
+    100%{background-position:0% 14%}
 }
-@keyframes AnimationName {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+@keyframes AnimationName { 
+    0%{background-position:0% 14%}
+    50%{background-position:100% 87%}
+    100%{background-position:0% 14%}
 }
 `;
 
@@ -40,7 +40,7 @@ function ParticlesView(){
             params={{
               "particles": {
                   "number": {
-                      "value": 150,
+                      "value": 250,
                       "density": {
                           "enable": true,
                           "value_area": 1803.4120608655228
--- src/components/Home/TitleName.jsx
+++ src/components/Home/TitleName.jsx
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 import HomeMenu from './HomeMenu';
 import DrawerRight from './DrawerRight';
 import Draggable from 'react-draggable';
-
+import ChevronDown from '../../assets/ChevronDown.svg';
+import ArrowDown from '../../assets/ArrowDown.png';
 
 const BodyWrapper = styled.section`
   border-radius: 30px;
@@ -13,14 +14,13 @@ const BodyWrapper = styled.section`
   margin: 0 auto;
   margin-top: 10%;
   padding: 20px;
-  opacity: 0.7;
+  opacity: 0.9;
   color: white;
 `;
 
 const Icons = styled.section `
   display: flex;
   justify-content: space-around;
-  margin-top: 40px;
 `;
 
 const Title = styled.h1`
@@ -32,13 +32,25 @@ const Title = styled.h1`
   `;
   
 const Tagline= styled.h1`
-  margin-top: -5;
   font-size: 40px;
   color: white;
   text-align: center;
   @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans+Condensed:400,400i|IBM+Plex+Sans:100,100i,400,400i,700,700i|IBM+Plex+Serif:400,400i');
   font-family: 'IBM Plex Sans', sans-serif;
   `;
+  
+const Link = styled.a`
+  color: white;
+  font-size: 30px;
+  text-decoration: none;
+`;
+
+const Arrow = styled.img`
+  height: 100px;
+  position: fixed;
+  bottom: 0;
+  left: 50%
+`;
   
 
 function Body(){
@@ -47,12 +59,14 @@ function Body(){
         <Title> Hello, I'm Devin! </Title>
         <Tagline> All-Star Dev | Code Fanatic | Awesome Human | Bleh</Tagline>
         <Icons>  
-        <a target="_blank" href="https://github.com/flexdinesh">Github |</a>
-  			<a target="_blank" href="https://twitter.com/flexdinesh">| Twitter |</a>
-  			<a target="_blank" href="https://www.linkedin.com/in/dineshpandiyan">| linkedin |</a>
-  			<a target="_blank" href="https://www.freecodecamp.org">| FaceBook |</a>
-  			<a target="_blank" href="https://www.behance.net">| Email |</a>
+        <Link target="_blank" href="https://github.com/flexdinesh">Github |</Link>
+  			<Link target="_blank" href="https://twitter.com/flexdinesh">| Twitter |</Link>
+  			<Link target="_blank" href="https://www.linkedin.com/in/dineshpandiyan">| linkedin |</Link>
+  			<Link target="_blank" href="https://www.freecodecamp.org">| FaceBook |</Link>
+  			<Link target="_blank" href="https://www.behance.net">| Email |</Link>
         </Icons>
+        
+        <Arrow src={ArrowDown}/>
       </BodyWrapper>
   );
 }
